15f2b8727cb69e480242bde5048341a98caf7cd4
Mint <demonheart752005@gmail.com>
2025-10-18 01:08:31 +0700

    fix booking service

--- src/main/java/service/BookingService.java
+++ src/main/java/service/BookingService.java
@@ -5,16 +5,13 @@ import org.springframework.data.domain.Pageable;
 import entity.Booking;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BookingService {
     List<Booking> getAllBookings();
-    Optional<Booking> getBookingById(String bookingId);
-    Booking saveBooking(Booking booking);
-    Booking updateBooking(String BookingId, Booking updateBooking);
+    List<Booking> getBookingById(String bookingId);
+    Booking saveBooking(Booking bookingId);
+    Booking updateBooking(String bookingId, Booking updateBooking);
     void deleteBookingById(String bookingId);
     List<Booking> searchBookings(String bookingId, String customerId, String roomId);
-    List<Booking> findByFilters(String bookingId, String customerId, String roomId, String status);
-    List<Booking> getAvailableBookings();
     Page<Booking> getAllBookings(Pageable pageable);
 }
