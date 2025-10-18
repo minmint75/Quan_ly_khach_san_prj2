acfcd63ac4bbc6b23223ccab8e155530a1fbc167
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2025-10-19 01:05:08 +0700

    Tạo nhánh service employe

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package service;
+
+import entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(Long id);
+    Employee saveEmployee(Employee employee);
+    Employee updateEmployee(Long id, Employee updatedEmployee);
+    void deleteEmployeeById(Long id);
+    List<Employee> searchEmployees(String name, String role, String email);
+    Page<Employee> getAllEmployees(Pageable pageable);
+    Page<Employee> sortEmployees(String sortBy, boolean ascending, Pageable pageable);
+}
