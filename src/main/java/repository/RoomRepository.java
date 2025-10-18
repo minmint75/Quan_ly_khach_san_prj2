a8dd956b008516d40d229c9a4b98b4f5321f3d27
jotoki3142 <shinaki3142@gmail.com>
2025-10-18 17:34:04 +0700

    Add RoomRepository and updated entity/Room

---
+++ src/main/java/repository/RoomRepository.java
@@ -0,0 +1,48 @@
+package repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import entity.Room;
+
+import java.util.List;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, String> {
+
+    List<Room> findByRoomNumberIgnoreCase(String roomNumber);
+
+    List<Room> findByRoomTypeIgnoreCase(String roomType);
+
+    List<Room> findByRoomFloor(int roomFloor);
+
+    @Query("SELECT r FROM Room r WHERE r.status = 'AVAILABLE' ORDER BY r.roomId, r.roomNumber")
+    List <Room> findAvailableRoom();
+
+    @Query("SELECT r FROM Room r WHERE " +
+            "(:roomNumber IS NULL OR r.roomNumber = :roomNumber) AND " +
+            "(:roomType IS NULL OR r.roomType = :roomType) AND " +
+            "(:roomFloor IS NULL OR r.roomFloor = :roomFloor) " +
+            "ORDER BY r.roomId DESC")
+    List<Room> findByFilters(@Param("roomNumber") String roomNumber,
+                             @Param("roomType") String roomType,
+                             @Param("roomFloor") Integer roomFloor);
+
+    @Query("SELECT r FROM Room r ORDER BY r.roomId DESC")
+    List<Room> findAllRoomsByIdDesc();
+
+    Page<Room> findAll(Pageable pageable);
+
+    @Query("SELECT r FROM Room r WHERE " +
+            "(:roomNumber IS NULL OR r.roomNumber = :roomNumber) AND " +
+            "(:roomType IS NULL OR r.roomType = :roomType) AND " +
+            "(:roomFloor IS NULL OR r.roomFloor = :roomFloor)")
+    Page<Room> findByFiltersPageable(@Param("roomNumber") String roomNumber,
+                                     @Param("roomType") String roomType,
+                                     @Param("roomFloor") Integer roomFloor,
+                                     Pageable pageable);                               
+
+}
